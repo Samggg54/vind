dc82a444989214b773ec7bb1bf5db4fb5f0746dd
Samggg54 <153571368+Samggg54@users.noreply.github.com>
2025-10-02 14:56:57 +0100

    Create NodeEditor.js

---
+++ components/NodeEditor.js
@@ -0,0 +1,8 @@
+export default function NodeEditor() {
+  return (
+    <div style={{ padding: "20px", border: "2px solid green" }}>
+      <h2>Vind Node Editor</h2>
+      <p>This is where nodes will appear.</p>
+    </div>
+  );
+}
